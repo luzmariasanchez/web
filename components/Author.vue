4adbd1ac57653b6b1432f3b7929a73b112502bd1
R. Romain <molosc@gmail.com>
2022-09-02 22:39:41 -0400

    author mobile

--- components/Author.vue
+++ components/Author.vue
@@ -1,21 +1,19 @@
 <template>
 
-  <!--Author-->
-  <div class="flex w-full items-center font-sans p-8 md:p-24">
+  <div class="flex w-full flex-col md:flex-row items-center font-sans p-8 md:p-24 border-t border-gray-800 mt-10">
     <img class="w-10 h-10 rounded-full mr-4" :src="$t('author.avatar')" alt="Avatar of Author">
-    <div class="flex-1">
-      <p class="text-base font-bold text-base md:text-xl leading-none">{{ $t('author.title') }}</p>
-      <p class="text-gray-600 text-xs md:text-base">{{ $t('author.content') }}</p>
+    <div class="flex-1 mb-5">
+      <p class="text-base font-bold text-base md:text-xl leading-none">{{  $t('author.title')  }}</p>
+      <p class="text-gray-600 text-xs md:text-base">{{  $t('author.content')  }}</p>
     </div>
     <div class="justify-end">
-      <nuxt-link as="button" :to="localePath('info')"
+      <nuxt-link as="button" :to="localePath('bio')"
         class="bg-transparent border border-gray-500 hover:border-green-500 text-xs text-gray-500 hover:text-green-500 font-bold py-2 px-4 rounded-full">
         {{
-            $t('author.more')
+         $t('author.more') 
         }}</nuxt-link>
     </div>
   </div>
-  <!--/Author-->
 
 </template>
 
