e8f5be3b5fd441a67217145635974b2babca7e37
R. Romain <molosc@gmail.com>
2022-09-11 02:15:37 -0400

    add active state to menu links

--- components/Head.vue
+++ components/Head.vue
@@ -15,7 +15,7 @@
               </li>
               <li>
                 <nuxt-link :to="localePath({ name: 'index' })"
-                  class="flex text-white text-base no-underline hover:text-white hover:no-underline pr-5" href="#">
+                  class="flex text-white text-base no-underline hover:text-white hover:no-underline pr-5">
                   <span class="w-0 w-auto block pl-1">Luz María Sánchez</span>
                 </nuxt-link>
               </li>
@@ -28,28 +28,28 @@
             <ul class="list-reset flex flex-col lg:flex-row lg:flex-1 lg:items-center" @click="closeMenu">
               <li class="mr-2">
                 <nuxt-link :to="localePath({ name: 'artworks' })"
-                  class="inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2"
-                  href="#">{{  $t('page.artworks')  }}</nuxt-link>
+                  class="cursor-pointer inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2">
+                  {{ $t('page.artworks') }}</nuxt-link>
               </li>
               <li class="mr-2">
                 <nuxt-link :to="localePath({ name: 'exhibitions' })"
-                  class="inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2"
-                  href="#">{{  $t('page.exhibitions')  }}</nuxt-link>
+                  class="cursor-pointer inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2">
+                  {{ $t('page.exhibitions') }}</nuxt-link>
               </li>
               <li class="mr-2">
                 <nuxt-link :to="localePath({ name: 'researchs' })"
-                  class="inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2"
-                  href="#">{{  $t('page.researchs')  }}</nuxt-link>
+                  class="cursor-pointer inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2">
+                  {{ $t('page.researchs') }}</nuxt-link>
               </li>
               <li class="mr-2">
                 <nuxt-link :to="localePath({ name: 'academias' })"
-                  class="inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2"
-                  href="#">{{  $t('page.academias')  }}</nuxt-link>
+                  class="cursor-pointer inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2">
+                  {{ $t('page.academias') }}</nuxt-link>
               </li>
               <li class="mr-2">
                 <nuxt-link :to="localePath({ name: 'publications' })"
-                  class="inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2"
-                  href="#">{{  $t('page.publications')  }}</nuxt-link>
+                  class="cursor-pointer inline-block text-gray-600 no-underline lg:hover:text-gray-200 hover:text-black hover:text-underline py-2 px-2">
+                  {{ $t('page.publications') }}</nuxt-link>
               </li>
             </ul>
           </div>
@@ -59,23 +59,24 @@
           <ul class="list-reset flex flex-1 items-center">
             <li class="ml-2">
               <nuxt-link :to="localePath('search')"
-                class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"><i
-                  class="icon-search"></i></nuxt-link>
+                class="cursor-pointer inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2">
+                <i class="icon-search"></i></nuxt-link>
             </li>
             <li class="ml-2">
               <nuxt-link :to="localePath('bio')"
-                class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"><i
-                  class="icon-info"></i></nuxt-link>
+                class="cursor-pointer inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2">
+                <i class="icon-info"></i></nuxt-link>
             </li>
             <li class="ml-2">
               <nuxt-link :to="localePath('contact')"
-                class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"><i
-                  class="icon-mail"></i></nuxt-link>
+                class="cursor-pointer inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2">
+                <i class="icon-mail"></i></nuxt-link>
             </li>
             <li class="ml-2">
               <nuxt-link :to="switchLocalePath(nextLocale.code)"
-                class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2">{{
-                 ('' + nextLocale.code).toUpperCase()  }}</nuxt-link>
+                class="cursor-pointer inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2">
+                {{
+                ('' + nextLocale.code).toUpperCase() }}</nuxt-link>
             </li>
           </ul>
         </div>
@@ -122,6 +123,10 @@ export default {
   font-size: 1.2em;
   background-color: white;
   z-index: 100;
+}
+
+.nuxt-link-active {
+  color: white;
 }
 </style>
   
