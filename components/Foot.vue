b949f66b5e5ddee98046d0d53ab14dff309121bf
R. Romain <molosc@gmail.com>
2022-09-11 02:10:56 -0400

    add social url in locale messages

--- components/Foot.vue
+++ components/Foot.vue
@@ -7,34 +7,38 @@
         <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
           <nuxt-link :to="localePath({ name: 'index' })"
             class="text-gray-200 no-underline hover:text-gray-400 hover:no-underline">
-            {{  $t('foot.title')  }}
+            {{ $t('foot.title') }}
           </nuxt-link>
         </div>
         <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
           <ul class="list-reset flex justify-center flex-1 md:flex-none items-center">
             <li>
               <nuxt-link :to="localePath('contact')"
-                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3" href="#">
+                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3">
                 <i class="icon-mail"></i>
               </nuxt-link>
             </li>
             <li>
-              <a class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3" href="#">
+              <a :href="$t('foot.social.linkedin')" target="_blank"
+                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3">
                 <i class="icon-linkedin"></i>
               </a>
             </li>
             <li>
-              <a class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3" href="#">
+              <a :href="$t('foot.social.twitter')" target="_blank"
+                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3">
                 <i class="icon-twitter"></i>
               </a>
             </li>
             <li>
-              <a class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3"
-                href="#"><i class="icon-instagram"></i></a>
+              <a :href="$t('foot.social.instagram')" target="_blank"
+                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3"><i
+                  class="icon-instagram"></i></a>
             </li>
             <li>
-              <a class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3"
-                href="#"><i class="icon-youtube"></i></a>
+              <a :href="$t('foot.social.youtube')" target="_blank"
+                class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:underline py-2 px-3"><i
+                  class="icon-youtube"></i></a>
             </li>
           </ul>
         </div>
