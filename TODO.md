11caef89e345488977e8dfb571f0bedc43465885
R. Romain <molosc@gmail.com>
2022-09-19 23:14:57 -0400

    fix author on mobile

--- TODO.md
+++ TODO.md
@@ -4,4 +4,6 @@ TODO
 - sitemap
 - private work
 - fix filter tags width on mobile
-- fix author on mobile
+- add works and remove categorys on media page
+- move media on another project
+- translate messages and remove notfound component
--- components/Author.vue
+++ components/Author.vue
@@ -1,9 +1,10 @@
 <template>
 
   <div class="flex w-full flex-col md:flex-row items-center font-sans p-8 md:p-24 border-t border-gray-800 mt-10">
-    <img class="w-16 h-16 rounded-full mr-4" :src="$imager($t('author.avatar'), 'square')" alt="Avatar of Author">
-    <div class="flex-1">
-      <p class="text-gray-200 font-bold text-base md:text-xl leading-none">{{ $t('author.title') }}</p>
+    <img class="w-16 h-16 rounded-full mb-4 md:mr-4 md:mb-0" :src="$imager($t('author.avatar'), 'square')"
+      alt="Avatar of Author">
+    <div class="flex-1 text-center md:text-left mb-4 md:mb-0">
+      <p class="text-gray-200 font-bold text-base md:text-xl leading-none mb-2 md:mb-0">{{ $t('author.title') }}</p>
       <p class="text-gray-600 text-xs md:text-base">{{ $t('author.content') }}</p>
     </div>
     <div class="justify-end">
